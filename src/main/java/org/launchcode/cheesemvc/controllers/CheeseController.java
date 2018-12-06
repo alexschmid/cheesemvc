1eee7985ac0435482057c6c13dc5b60a17bdd569
Alex Schmid <7277969+alexschmid@users.noreply.github.com>
2018-12-06 14:48:25 -0600

    Initial commit

---
+++ src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
@@ -0,0 +1,28 @@
+package org.launchcode.cheesemvc.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+
+@Controller
+@RequestMapping("cheese")
+public class CheeseController {
+
+    // Request path: /cheese
+    @RequestMapping(value = "")
+    public String index(Model model) {
+
+        ArrayList<String> cheeses = new ArrayList<>();
+        cheeses.add("cheddar");
+        cheeses.add("parmesan");
+        cheeses.add("munster");
+
+        model.addAttribute("cheeses", cheeses);
+        model.addAttribute("title", "My Cheeses");
+
+        return "cheese/index";
+    }
+
+}
