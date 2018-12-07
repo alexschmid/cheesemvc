4df4547777e7146654f7762bdb4ea55efa189ddf
Alex Schmid <7277969+alexschmid@users.noreply.github.com>
2018-12-07 11:34:22 -0600

    After 'Intro to Spring Boot: Models (Part 2)'

---
+++ src/main/java/org/launchcode/cheesemvc/models/CheeseData.java
@@ -0,0 +1,39 @@
+package org.launchcode.cheesemvc.models;
+
+import java.util.ArrayList;
+
+
+public class CheeseData {
+
+    static ArrayList<org.launchcode.models.Cheese> cheeses = new ArrayList<>();
+
+    // getAll
+    public static ArrayList<org.launchcode.models.Cheese> getAll() {
+        return cheeses;
+    }
+    // add
+    public static void  add(org.launchcode.models.Cheese newCheese) {
+        cheeses.add(newCheese);
+    }
+
+    // remove
+    public static void remove(int id) {
+        org.launchcode.models.Cheese cheeseToRemove = getById(id);
+        cheeses.remove(cheeseToRemove);
+    }
+
+    // getById
+    public static org.launchcode.models.Cheese getById(int id) {
+
+        org.launchcode.models.Cheese theCheese = null;
+
+        for (org.launchcode.models.Cheese candidateCheese : cheeses) {
+            if(candidateCheese.getCheeseId() == id) {
+                theCheese = candidateCheese;
+            }
+        }
+
+        return theCheese;
+
+    }
+}
